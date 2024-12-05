e38f283d0f7eb04d22eda1cb40afbc975c98cfc4
Arejas <62893479+arejasverduras@users.noreply.github.com>
2024-12-05 11:45:08 +0100

    add description & execution

--- src/app/components/ExerciseList/ExerciseList.tsx
+++ src/app/components/ExerciseList/ExerciseList.tsx
@@ -7,6 +7,8 @@ const fakeData:ExerciseProps[] = [
         title: 'Squats',
         image: '/images/exercises/JSquat.png',
         // video: '6-Pu6LhaTB8?si=4Qv7dN9saFD0Daez' 
+        description: 'Bend those knees!',
+        reps: '3 x 12 reps. Hold weights if easy'
     },
     {
         title: 'Frog',
--- src/app/components/ExerciseList/ExerciseListItems/Exercise/Exercise.tsx
+++ src/app/components/ExerciseList/ExerciseListItems/Exercise/Exercise.tsx
@@ -24,21 +24,26 @@ export const Exercise = ({data, index}:Exercise) => {
     }
 
     return (
-        <>
+        <div className="my-5 border">
             <h4 
-                className="my-5 border p-5 cursor-pointer"
+                className=" p-5 cursor-pointer"
                 onClick={toggle}>
                     {index+1}. {data.title}
             </h4>
-           { open && (<div className="">
-                <Image 
-                    src={data.image} 
-                    alt={data.title} 
-                    width="1600" 
-                    height="900"
-                />
-            {data.video && <YouTube embedId={data.video}/>}
-            </div>)}
-        </>
+           { open && (
+                <div className="">
+                    <Image 
+                        src={data.image} 
+                        alt={data.title} 
+                        width="1600" 
+                        height="900"
+                    />
+                    {data.video && <YouTube embedId={data.video}/>}
+                    {data.description && <div className="p-5">{data.description}</div>}
+                    {data.reps && 
+                       (<div className="p-5"><h5 className="font-bold">Execution</h5> <div>{data.reps}</div> </div>)
+                        }
+                </div>)}
+        </div>
     )
 }
