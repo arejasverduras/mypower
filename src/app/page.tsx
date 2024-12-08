2ea34cddcdfaea56875578416b089f96e4510ad7
Arejas <62893479+arejasverduras@users.noreply.github.com>
2024-12-08 13:33:49 +0100

    create basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 
-import { HeaderJara } from "./components/Header/Header";
-import { Hero } from "./components/Hero/Hero";
-import { ExerciseList } from "./components/ExerciseList/ExerciseList";
+
+import Link from "next/link";
 
 
 export default function Home() {
   
   return (
-    <div className=" min-h-screen bg-primary-color text-secondary-color">
-      <HeaderJara/>
-      <Hero/>
-      <ExerciseList/>
+    <div className=" min-h-screen bg-primary-color text-secondary-color p-5">
+      <h2 className="text-2xl my-5">Welcome to MyPower!</h2>
+      <Link 
+        href="/exercises"
+        className="font-bold"
+        >Go to exercises</Link>
     </div>
   );
 }
