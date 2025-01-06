f096d23685f002964f820b0a8e52a0803e8734b6
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-01-06 10:51:05 +0100

    add accelerate to prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,33 @@
+// import { PrismaClient } from "@prisma/client";
+// import createAccelerate
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// // Reuse Prisma Client instance if already defined
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ["query"], // Optional: Logs queries for debugging
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma; // Store in global object for dev mode
+// }
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// Reuse Prisma Client instance if already defined
 const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"], // Optional: Logs queries for debugging
-  });
+  new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma; // Store in global object for dev mode
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
+
