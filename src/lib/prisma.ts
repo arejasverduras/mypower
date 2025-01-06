67ddb3ff28998455b906f6d37204fed4c8e98b0f
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-01-06 10:59:23 +0100

    stick with unaccelerated prisma for now

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,33 +1,39 @@
-// import { PrismaClient } from "@prisma/client";
-// import createAccelerate
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-// // Reuse Prisma Client instance if already defined
-// const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: ["query"], // Optional: Logs queries for debugging
-//   });
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma; // Store in global object for dev mode
-// }
-
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// Reuse Prisma Client instance if already defined
 const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient().$extends(withAccelerate());
+  new PrismaClient({
+    log: ["query"], // Optional: Logs queries for debugging
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma; // Store in global object for dev mode
 }
 
 export default prisma;
 
+// import { PrismaClient } from "@prisma/client";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient().$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
+
+// export default prisma;
+
+// import { PrismaClient } from '@prisma/client'
+// import { withAccelerate } from '@prisma/extension-accelerate'
+
+// const prisma = new PrismaClient().$extends(withAccelerate())
+
+// export default prisma;
