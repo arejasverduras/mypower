00725c3fafa14c086cafa71693dc25e360ff0a51
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-01-07 10:05:25 +0100

    protect dashboard page

--- auth.ts
+++ auth.ts
@@ -18,5 +18,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       session.user.id = user.id;
       return session;
     },
+    // authorized: async({auth}) => {
+    //   return !!auth
+    // }
   },
 })
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 
 import { SignOutButton} from "../components/SignOutButton/SignOutButton"
 import UserAvatar from "../components/UserAvatar/UserAvatar"
+import { auth } from "../../../auth"
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const session = await auth()
+  if (!session) return <div>Not authenticated</div>
+
 
     return (
         <>
@@ -11,6 +15,8 @@ export default function DashboardPage() {
             <p>Hello logged in user!</p>
             <UserAvatar/>
             <SignOutButton/>
+            {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+
         </>
         
     )
