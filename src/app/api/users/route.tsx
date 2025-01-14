dfb591840e3ef135f88380a2fa82c687666b2a10
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-01-14 10:58:18 +0100

    refactor /users get request for proper typing

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,17 +1,37 @@
 import { NextResponse } from "next/server";
+import { auth } from "../../../../auth";
 import prisma from "@/lib/prisma";
 
-export async function GET () {
-    try {
-        const users = await prisma.user.findMany({
-            orderBy: {createdAt: "desc"}
-        })
+export const GET = auth(async (req) => {
+  if (!req.auth?.user) {
+    return NextResponse.json(
+      { error: "Unauthorized" },
+      { status: 401, statusText: "You must be logged in to perform this action" }
+    );
+  }
 
-        // if (!users) return NextResponse.json({error: "No users found"}, {status: 404});
+  try {
+    const isSuperuser = req.auth.user.isSuperuser;
 
-        return NextResponse.json({ users }, {status: 200})
-    } catch (error) {
-        // console.log(error)
-        return NextResponse.json({error: "Internal server error"}, {status: 500})
-    }
-};
+    const users = await prisma.user.findMany({
+      select: isSuperuser
+        ? undefined // Superusers can access all fields
+        : {
+            id: true,
+            name: true,
+            image: true,
+            createdAt: true,
+          }, // Regular users get limited fields
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+});
+
