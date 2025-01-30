159d2620218421041c0153afae631c24b4c03cb1
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-01-30 12:06:56 +0100

    first tests

---
+++ src/app/__tests__/page.test.tsx
@@ -0,0 +1,11 @@
+
+import { screen, render } from '@testing-library/react';
+
+import Home from '@/app/page';
+
+describe('<Home />', () => {
+  test('It renders', () => {
+    render(<Home />);
+    expect(screen.getByText(/mypower/i)).toBeInTheDocument();
+  });
+});
