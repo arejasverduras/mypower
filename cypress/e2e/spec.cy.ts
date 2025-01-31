6ed00124f363eb2f6dca79339a6703c207c1d30d
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-01-31 11:45:08 +0100

    working api test!!!

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
 
-describe('first test', () => {
-  it('visit host', () => {
-    cy.visit('http://localhost:3000')
+// describe('first test', () => {
+//   it('visit host', () => {
+//     cy.visit('http://localhost:3000')
+//   })
+// })
+
+describe('get test', () => {
+  it('gets exercises from /api/exercises', () => {
+    cy.request('GET', 'http://localhost:3000/api/exercises')
   })
 })
