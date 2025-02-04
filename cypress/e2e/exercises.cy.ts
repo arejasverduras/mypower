92b2b2f46c690a3475793b232fb14ab047aa609e
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-02-04 09:24:10 +0100

    exercises tests

--- cypress/e2e/exercises.cy.ts
+++ cypress/e2e/exercises.cy.ts
@@ -6,33 +6,33 @@ describe ('Exercises page', () => {
     
    
    
-    // it('should display a list of exercises', () => {
-    //     cy.visit('http://localhost:3000/exercises')
-    //     // cy.get('#exercise-list-items').should('exist')
-    //     cy.get('#exercise-list-items').find('div').should('have.length.to.be.greaterThan', 5)
+    it('should display a list of exercises', () => {
+        cy.visit('http://localhost:3000/exercises')
+        // cy.get('#exercise-list-items').should('exist')
+        cy.get('#exercise-list-items').find('div').should('have.length.to.be.greaterThan', 5)
 
-    // })
+    })
 
-    // it ('clicking a list items should display a read more button', () => {
-    //     cy.visit('http://localhost:3000/exercises')
-    //     cy.get('#exercise-list-items').find('div').first().click()
-    //     cy.get('button').contains('Read more').should('exist')
-    // })
+    it ('clicking a list items should display a read more button', () => {
+        cy.visit('http://localhost:3000/exercises')
+        cy.get('#exercise-list-items').find('div').first().click()
+        cy.get('button').contains('Read more').should('exist')
+    })
 
-    // it ('clicking a read more button should display the exercise details', () => {
-    //     cy.visit('http://localhost:3000/exercises')
-    //     cy.get('#exercise-list-items').find('div').first().click()
-    //     cy.get('button').contains('Read more').click()
-    //     cy.contains('Go Back to exercises').should('exist')
-    // })
+    it ('clicking a read more button should display the exercise details', () => {
+        cy.visit('http://localhost:3000/exercises')
+        cy.get('#exercise-list-items').find('div').first().click()
+        cy.get('button').contains('Read more').click()
+        cy.contains('Go Back to exercises').should('exist')
+    })
 
-    // it ('clicking the back button should return to the exercises page', () => {
-    //     cy.visit('http://localhost:3000/exercises')
-    //     cy.get('#exercise-list-items').find('div').first().click()
-    //     cy.get('button').contains('Read more').click()
-    //     cy.get('button').contains('Go Back to exercises').click()
-    //     cy.contains('exercises').should('exist')
-    // })
+    it ('clicking the back button should return to the exercises page', () => {
+        cy.visit('http://localhost:3000/exercises')
+        cy.get('#exercise-list-items').find('div').first().click()
+        cy.get('button').contains('Read more').click()
+        cy.get('button').contains('Go Back to exercises').click()
+        cy.contains('exercises').should('exist')
+    })
 
     it('should display a message when there are no exercises', () => {
         cy.intercept('GET', '/api/exercises', { body: [] }).as('getExercises')
