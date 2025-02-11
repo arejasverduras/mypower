7dcb22288955627b2e740b485b5eddf5df230288
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-02-11 11:13:58 +0100

    get handler for workout route, mockdata

---
+++ src/app/api/workouts/route.tsx
@@ -0,0 +1,10 @@
+// import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import workouts from'../../../../cypress/fixtures/workouts.json';
+
+
+// GET requests
+export async function GET() {
+
+    return NextResponse.json(workouts, {status: 200});
+}
