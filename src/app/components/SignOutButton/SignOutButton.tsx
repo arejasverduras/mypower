234af31efb6f9ba183a0f9c13ffe912b61537322
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-03-21 10:10:59 +0100

    signOut redirect to route

--- src/app/components/SignOutButton/SignOutButton.tsx
+++ src/app/components/SignOutButton/SignOutButton.tsx
@@ -6,7 +6,7 @@ export function SignOutButton() {
   return (
     <button
       type="button"
-      onClick={() => signOut()}
+      onClick={() => signOut({ callbackUrl: "/" })}
       className="px-4 py-2 bg-red-500 text-white rounded"
     >
       Sign Out
