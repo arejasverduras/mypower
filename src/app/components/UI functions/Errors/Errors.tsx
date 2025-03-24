dcd62b14de32d616562cbebe6296fbd2b5838655
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-03-24 11:02:48 +0100

    style error component

--- src/app/components/UI functions/Errors/Errors.tsx
+++ src/app/components/UI functions/Errors/Errors.tsx
@@ -8,9 +8,9 @@ interface ErrorsProps {
   
   export const Errors = ({ errors }:ErrorsProps) => {
     return (
-      <>
+      <div className="space-y-4">
         {errors.length > 0 && errors.map((error, index) => <ErrorItem key={index} error={error} />)}
-      </>
+      </div>
     );
   };
   
