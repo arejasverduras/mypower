7a0504a521ff2681328b498752687aed7f6f7459
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-03-31 11:18:37 +0200

    updated patch route for users/id/edit

--- src/app/api/users/[id]/edit/route.tsx
+++ src/app/api/users/[id]/edit/route.tsx
@@ -1,87 +1,80 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { auth } from "../../../../../../auth";
-import { User } from "next-auth";
+// import { auth } from "../../../../../../auth";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../../../../auth";
 
-export async function PATCH(
-    req: NextRequest,
-    context: { params: { id: string } },
-  ): Promise<Response> 
 
-  {
-    return auth(async (
-        authreq: any & { auth?: { user?: User } }
-    ) => {
-        const { id } = await context.params;
+// export async function PATCH(
+//     req: NextRequest,
+//     context: { params: { id: string } },
+//   ): Promise<Response> 
+
+//   {
+//     return auth(async (
+//         authreq: any & { auth?: { user?: User } }
+//     ) => {
+//         const { id } = await context.params;
   
-    // 1. checks for a logged in user
-        if (!authreq.auth?.user) {
-        return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-        }
+//     // 1. checks for a logged in user
+//         if (!authreq.auth?.user) {
+//         return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+//         }
 
-    //   2. check if the user exists
-    const existingUser = await prisma.user.findUnique({
-        where: { id }
-    })
+//     //   2. check if the user exists
+//     const existingUser = await prisma.user.findUnique({
+//         where: { id }
+//     })
 
-    if (!existingUser) {
-        return NextResponse.json({ error: "User not found"}, { status: 404})
-    }
+//     if (!existingUser) {
+//         return NextResponse.json({ error: "User not found"}, { status: 404})
+//     }
 
-    // 3. Check if the user is the creator or a superuser
-    const isSelfOrSuperuser =
-            authreq?.auth?.user.id === id || authreq?.auth?.user.isSuperuser;
+//     // 3. Check if the user is the creator or a superuser
+//     const isSelfOrSuperuser =
+//             authreq?.auth?.user.id === id || authreq?.auth?.user.isSuperuser;
 
-    if (!isSelfOrSuperuser) {
-        return NextResponse.json({ error: "Not authorized" }, { status: 403 });
-    }
+//     if (!isSelfOrSuperuser) {
+//         return NextResponse.json({ error: "Not authorized" }, { status: 403 });
+//     }
 
-    const body = await req.json();
+//     const body = await req.json();
 
-    // 4. Update the user
-        try {
-            const updatedUser = await prisma.user.update({
-              where: { id },
-              data: body,
-              include: {
-                createdExercises: true,
-              }
-            });
+//     // 4. Update the user
+//         try {
+//             const updatedUser = await prisma.user.update({
+//               where: { id },
+//               data: body,
+//               include: {
+//                 createdExercises: true,
+//               }
+//             });
 
-            if (!updatedUser) return NextResponse.json({error: "Update failed"}, { status: 400})
+//             if (!updatedUser) return NextResponse.json({error: "Update failed"}, { status: 400})
   
-        return NextResponse.json(updatedUser, { status: 200 });
-      } catch (error) {
-        console.error(error);
-        return NextResponse.json(
-          { error: "Internal Server Error" },
-          { status: 500 }
-        );
-      }
-    }
-            )(req, context) as Promise<Response>;
-  }
+//         return NextResponse.json(updatedUser, { status: 200 });
+//       } catch (error) {
+//         console.error(error);
+//         return NextResponse.json(
+//           { error: "Internal Server Error" },
+//           { status: 500 }
+//         );
+//       }
+//     }
+//             )(req, context) as Promise<Response>;
+  // }
  
-
-export async function DELETE(
-  req: NextRequest,
-  context: { params: { id: string } },
-): Promise<Response> 
-
-{
-  return auth(async (
-      authreq: any & { auth?: { user?: User } }
-  ) => {
-      const { id } = await context.params;
-
-  // 1. checks for a logged in user
-      if (!authreq.auth?.user) {
+  export const PATCH = async (req: Request, {params}: {params: {id:string}}) => {
+    const session = await getServerSession(authOptions);
+  
+    if (!session) {
       return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-      }
-
-  //   2. check if the user exists
-  const existingUser = await prisma.user.findUnique({
+    }
+  
+    const { id } = params;
+  
+      //   2. check if the user exists
+    const existingUser = await prisma.user.findUnique({
       where: { id }
   })
 
@@ -91,30 +84,88 @@ export async function DELETE(
 
   // 3. Check if the user is the creator or a superuser
   const isSelfOrSuperuser =
-          authreq?.auth?.user.id === id || authreq?.auth?.user.isSuperuser;
+          session.user.id === id || session.user.isSuperuser;
 
   if (!isSelfOrSuperuser) {
       return NextResponse.json({ error: "Not authorized" }, { status: 403 });
   }
+  
+    const body = await req.json();
+  
+    // 4. Update the user
+    try {
+      const updatedUser = await prisma.user.update({
+        where: { id },
+        data: body,
+        include: {
+          createdExercises: true,
+        }
+      });
 
-  // 4. Deletethe user
-      try {
+      if (!updatedUser) return NextResponse.json({error: "Update failed"}, { status: 400})
 
-        await prisma.user.delete({
-          where: {id}
-        })
-
-
-      return NextResponse.json({message: "User deleted succesfully"}, { status: 200 });
-
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json(
-        { error: "Internal Server Error" },
-        { status: 500 }
-      );
-    }
-  }
-          )(req, context) as Promise<Response>;
+  return NextResponse.json(updatedUser, { status: 200 });
+} catch (error) {
+  console.error(error);
+  return NextResponse.json(
+    { error: "Internal Server Error" },
+    { status: 500 }
+  );
 }
+  
+  }
+
+// export async function DELETE(
+//   req: NextRequest,
+//   context: { params: { id: string } },
+// ): Promise<Response> 
+
+// {
+//   return auth(async (
+//       authreq: any & { auth?: { user?: User } }
+//   ) => {
+//       const { id } = await context.params;
+
+//   // 1. checks for a logged in user
+//       if (!authreq.auth?.user) {
+//       return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+//       }
+
+//   //   2. check if the user exists
+//   const existingUser = await prisma.user.findUnique({
+//       where: { id }
+//   })
+
+//   if (!existingUser) {
+//       return NextResponse.json({ error: "User not found"}, { status: 404})
+//   }
+
+//   // 3. Check if the user is the creator or a superuser
+//   const isSelfOrSuperuser =
+//           authreq?.auth?.user.id === id || authreq?.auth?.user.isSuperuser;
+
+//   if (!isSelfOrSuperuser) {
+//       return NextResponse.json({ error: "Not authorized" }, { status: 403 });
+//   }
+
+//   // 4. Deletethe user
+//       try {
+
+//         await prisma.user.delete({
+//           where: {id}
+//         })
+
+
+//       return NextResponse.json({message: "User deleted succesfully"}, { status: 200 });
+
+//     } catch (error) {
+//       console.error(error);
+//       return NextResponse.json(
+//         { error: "Internal Server Error" },
+//         { status: 500 }
+//       );
+//     }
+//   }
+//           )(req, context) as Promise<Response>;
+// }
 
