fbd9d13a3c40ab607c8ae792742d4e27944c833b
Arejas <62893479+arejasverduras@users.noreply.github.com>
2025-04-03 10:52:24 +0200

    add exercise to workout route

--- src/app/api/workouts/[id]/edit/route.tsx
+++ src/app/api/workouts/[id]/edit/route.tsx
@@ -1,199 +1,236 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { auth } from "../../../../../../auth";
-import { User } from "next-auth";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../../../../auth";
 
+// Add an exercise to a workout
+export async function POST(req: Request, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
 
-// add exercise to workout
-export async function POST(
-    req: NextRequest,
-    context: { params: { id: string } },
-  ): Promise<NextResponse> 
-
-  {
-    return auth(async (
-
-        authreq: any & { auth?: { user?: User } }
-    ) => {
-        const { id } = await context.params;
-  
-    // 1. checks for a logged in user
-        if (!authreq.auth?.user) {
-        return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-        }
-
-    //   2. check if the user exists / workout
-    const existingWorkout = await prisma.workout.findUnique({
-        where: { id }
-    })
-
-    if (!existingWorkout) {
-        return NextResponse.json({ error: "Workout not found"}, { status: 404})
-    }
-
-    // 3. Check if the user is the creator or a superuser
-    const isSelfOrSuperuser =
-            authreq?.auth?.user.id === id || authreq?.auth?.user.isSuperuser;
-
-    if (!isSelfOrSuperuser) {
-        return NextResponse.json({ error: "Not authorized" }, { status: 403 });
-    }
-
-    const { exerciseId } = await req.json();
-
-
-    // 4. Update the workout
-    try {
-        const updatedWorkout = await prisma.workout.update({
-          where: { id },
-          data: {
-            exercises: {
-              create: {
-                exerciseId,
-              },
-            },
-          },
-          include: {
-            exercises: { include: { exercise: { include: {createdBy: true}} } }
-          },
-        });
-    
-        return NextResponse.json(updatedWorkout, { status: 200 });
-      } catch (error) {
-        console.log(error);
-        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-      }
-    }
-            )(req, context) as Promise<NextResponse>;
+  if (!session) {
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
   }
 
+  const { id } = params;
 
-  // edit metadata
-export async function PATCH(req: NextRequest,context: { params: { id: string } },): Promise<Response> 
-
-{
-  return auth(async (
-
-      authreq: any & { auth?: { user?: User } }
-  ) => {
-      const { id } = await context.params;
-
-  // 1. checks for a logged in user
-      if (!authreq.auth?.user) {
-      return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-      }
-
-  //   2. check if the workout exists
+  // Check if the workout exists
   const existingWorkout = await prisma.workout.findUnique({
-      where: { id }
-  })
+    where: { id },
+  });
 
   if (!existingWorkout) {
-      return NextResponse.json({ error: "Workout not found"}, { status: 404})
+    return NextResponse.json({ error: "Workout not found" }, { status: 404 });
   }
 
-  // 3. Check if the user is the creator or a superuser
+  // Check if the user is the creator or a superuser
   const isSelfOrSuperuser =
-          authreq?.auth?.user.id === id || authreq?.auth?.user.isSuperuser;
+    session.user.id === existingWorkout.createdById || session.user.isSuperuser;
 
   if (!isSelfOrSuperuser) {
-      return NextResponse.json({ error: "Not authorized" }, { status: 403 });
+    return NextResponse.json({ error: "Not authorized" }, { status: 403 });
   }
 
-  const { body } = await req.json();
-
-
-  // 4. Update the workout
   try {
-      const updatedWorkout = await prisma.workout.update({
-        where: { id },
-        data: {
-          title: body.title,
-          description: body.description
-        },
-        include: {
-          createdBy: true,
-          tags: true,
-          exercises: { include: { exercise: { include: {createdBy: true}} } },
-          likedBy: true,
-          programs: true,
-        },
-      });
-  
-      return NextResponse.json(updatedWorkout, { status: 200 });
-    } catch (error) {
-      console.log(error);
-      return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-    }
-  }
-          )(req, context) as Promise<Response>;
-}
-
-
-
-  export async function DELETE(
-    req: NextRequest,
-    context: { params: { id: string } },
-  ): Promise<Response> {
-
-    return auth(async (authreq: any & { auth?: { user?: User } }) => {
-      const { id } = await context.params;
-
-
-    // 1. checks for a logged in user
-        if (!authreq.auth?.user) {
-        return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-        }
-
-    //   2. check if the workout exists
-    const existingWorkout = await prisma.workout.findUnique({
-        where: { id }
-    })
-
-    if (!existingWorkout) {
-        return NextResponse.json({ error: "Workout not found"}, { status: 404})
-    }
-
-    // 3. Check if the user is the creator or a superuser
-    const isSelfOrSuperuser =
-            authreq?.auth?.user.id === existingWorkout.createdById || authreq?.auth?.user.isSuperuser;
-
-    if (!isSelfOrSuperuser) {
-        return NextResponse.json({ error: "Not authorized" }, { status: 403 });
-    }
-
     const { exerciseId } = await req.json();
 
-    // 4. Delete the exercise from the workout
-        try {
-          const updatedWorkout = 
-          await prisma.workout.update({
-            where: { id },
-            data: {
-              exercises: {
-                delete: {
-                  id: exerciseId,
-                },
-              },
-            },
-            include: {
-              exercises: { include: { exercise: { include: { createdBy: true } } } }
+    // Add the exercise to the workout
+    const updatedWorkout = await prisma.workout.update({
+      where: { id },
+      data: {
+        exercises: {
+          create: {
+            exerciseId,
           },
+        },
+      },
+      include: {
+        exercises: { include: { exercise: { include: { createdBy: true } } } },
+      },
+    });
 
-          });
-
-          // updatedWorkout.message = "Exercise deleted";
-
-          return NextResponse.json(updatedWorkout, { status: 200 });
-
-      } catch (error) {
-        console.error(error);
-        return NextResponse.json(
-          { error: "Internal Server Error" },
-          { status: 500 }
-        );
-      }
-      
-    })(req, context) as Promise<Response>;
+    return NextResponse.json(updatedWorkout, { status: 200 });
+  } catch (error) {
+    console.error("Error adding exercise to workout:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
+}
+
+// Edit exercise metadata in a workout
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+  }
+
+  const { id } = params;
+
+  // Check if the workout exists
+  const existingWorkout = await prisma.workout.findUnique({
+    where: { id },
+  });
+
+  if (!existingWorkout) {
+    return NextResponse.json({ error: "Workout not found" }, { status: 404 });
+  }
+
+  // Check if the user is the creator or a superuser
+  const isSelfOrSuperuser =
+    session.user.id === existingWorkout.createdById || session.user.isSuperuser;
+
+  if (!isSelfOrSuperuser) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 403 });
+  }
+
+  try {
+    const { exerciseId, metadata } = await req.json();
+
+    // Update the exercise metadata in the workout
+    const updatedWorkout = await prisma.workoutExercise.update({
+      where: {
+        workoutId_exerciseId: {
+          workoutId: id,
+          exerciseId,
+        },
+      },
+      data: {
+        ...metadata,
+      },
+      include: {
+        workout: {
+          include: {
+            exercises: { include: { exercise: { include: { createdBy: true } } } },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(updatedWorkout, { status: 200 });
+  } catch (error) {
+    console.error("Error updating exercise metadata:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+// Change the order of exercises in a workout
+// export async function PUT(req: Request, { params }: { params: { id: string } }) {
+//   const session = await getServerSession(authOptions);
+
+//   if (!session) {
+//     return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+//   }
+
+//   const { id } = params;
+
+//   // Check if the workout exists
+//   const existingWorkout = await prisma.workout.findUnique({
+//     where: { id },
+//   });
+
+//   if (!existingWorkout) {
+//     return NextResponse.json({ error: "Workout not found" }, { status: 404 });
+//   }
+
+//   // Check if the user is the creator or a superuser
+//   const isSelfOrSuperuser =
+//     session.user.id === existingWorkout.createdById || session.user.isSuperuser;
+
+//   if (!isSelfOrSuperuser) {
+//     return NextResponse.json({ error: "Not authorized" }, { status: 403 });
+//   }
+
+//   try {
+//     const { exerciseOrder } = await req.json();
+
+//     if (!Array.isArray(exerciseOrder)) {
+//       return NextResponse.json({ error: "Invalid exercise order format" }, { status: 400 });
+//     }
+
+//     // Update the order of exercises
+//     await Promise.all(
+//       exerciseOrder.map((exerciseId: string, index: number) =>
+//         prisma.workoutExercise.update({
+//           where: {
+//             workoutId_exerciseId: {
+//               workoutId: id,
+//               exerciseId,
+//             },
+//           },
+//           data: {
+//             order: index + 1, // Set the order based on the array index
+//           },
+//         })
+//       )
+//     );
+
+//     // Fetch the updated workout with the new order
+//     const updatedWorkout = await prisma.workout.findUnique({
+//       where: { id },
+//       include: {
+//         exercises: {
+//           include: { exercise: { include: { createdBy: true } } },
+//           orderBy: { order: "asc" }, // Ensure the exercises are returned in the correct order
+//         },
+//       },
+//     });
+
+//     return NextResponse.json(updatedWorkout, { status: 200 });
+//   } catch (error) {
+//     console.error("Error updating exercise order:", error);
+//     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+//   }
+// }
+
+
+// Delete an exercise from a workout
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+  }
+
+  const { id } = params;
+
+  // Check if the workout exists
+  const existingWorkout = await prisma.workout.findUnique({
+    where: { id },
+  });
+
+  if (!existingWorkout) {
+    return NextResponse.json({ error: "Workout not found" }, { status: 404 });
+  }
+
+  // Check if the user is the creator or a superuser
+  const isSelfOrSuperuser =
+    session.user.id === existingWorkout.createdById || session.user.isSuperuser;
+
+  if (!isSelfOrSuperuser) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 403 });
+  }
+
+  try {
+    const { exerciseId } = await req.json();
+
+    // Delete the exercise from the workout
+    const updatedWorkout = await prisma.workout.update({
+      where: { id },
+      data: {
+        exercises: {
+          delete: {
+            id: exerciseId,
+          },
+        },
+      },
+      include: {
+        exercises: { include: { exercise: { include: { createdBy: true } } } },
+      },
+    });
+
+    return NextResponse.json(updatedWorkout, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting exercise from workout:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
